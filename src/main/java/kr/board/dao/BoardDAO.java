9434ca2864b988829555fab516e5319a7cf3916b
user <user@DESKTOP-COJ9858>
2023-11-22 15:52:07 +0900

    11.22

--- src/main/java/kr/board/dao/BoardDAO.java
+++ src/main/java/kr/board/dao/BoardDAO.java
@@ -189,4 +189,26 @@ public class BoardDAO {
 	}
 	
 	//글 삭제
+	public void delete(int num)throws Exception{
+		Connection conn= null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		
+		try {
+			//커넥션풀로부터 커넥션을 할당
+			conn = DBUtil.getConnection();
+			//sql문 작성
+			sql = "DELETE FROM svboard WHERE num=?";
+			//preparedStatement 객체 생성
+			pstmt = conn.prepareStatement(sql);
+			//?에 데이터 바인딩
+			pstmt.setInt(1, num);
+			//sql문 실행
+			pstmt.executeUpdate();
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(null, pstmt, conn);
+		}
+	}
 }
